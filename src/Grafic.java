5dc7fb3549ccb3d4ca0638c8b97791b56b989052
amenvafan <elisabet_lilja@yahoo.se>
2017-05-05 13:18:45 +0200

    uppd.

---
+++ src/Grafic.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+
+public class Grafic {
+
+	public static void main(String[]args){
+		
+		StdDraw.setCanvasSize(800, 800);
+		
+		//StdDraw.picture(0.5, 0.5, korsning.jpg.jpg);
+		Picture p = new Picture();
+		
+	}
+
+	
+}
