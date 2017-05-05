96e7f4aed6fc788152ebc4b7b71b1a03d1b78892
flixon <flixon@me.com>
2017-05-05 10:09:28 +0200

    Update to car x, y

--- src/Car.java
+++ src/Car.java
@@ -2,9 +2,18 @@ import java.awt.Color;
 
 public class Car {
 	
+	/**
+	 * - Car class. 
+	 * - To add X and Y cords.
+	 * - 
+	 */
+	
 	private boolean isEmergency;
 	private Color carColor;
 	private int delay;
+	private int x;
+	private int y;
+	
 	
 	public Car(boolean isEmergency, Color carColor, int delay){
 		this.isEmergency = isEmergency;
