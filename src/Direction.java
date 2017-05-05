7f2f423dacaf52cf534af354b68ecca551618de5
Niner0 <daniel_ahlsen@hotmail.com>
2017-05-05 10:55:30 +0200

    hasEmergency

--- src/Direction.java
+++ src/Direction.java
@@ -1,49 +1,53 @@
 import java.util.Queue;
 
-public class Direction implements Runnable{
+public class Direction implements Runnable {
 	private Queue<Car> straight;
 	private Queue<Car> turnLeft;
 	private Queue<Car> emergency;
 	private Light light;
-	
-	public Direction(){
+
+	public Direction() {
 		light = new Light();
 	}
-	
-	public void run(){
-		while(light.getGreen()){
-			this.greenLight();
+
+	public void run() {
+		while (light.isGreen()) {
+			try {
+				this.greenLight();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-	public void enqueueLeft(Car car){
-		if(car.isEmergency()){
+
+	public void enqueueLeft(Car car) {
+		if (car.isEmergency()) {
 			emergency.add(car);
 		}
 		turnLeft.add(car);
-		
+
 	}
-	
-	public void enqueueStraight(Car car){
-		if(car.isEmergency()){
+
+	public void enqueueStraight(Car car) {
+		if (car.isEmergency()) {
 			emergency.add(car);
 		}
 		straight.add(car);
-		
+
 	}
-	
-	public void greenLight(){
-			straight.remove();
-			if(light.getRed()){
-				turnLeft.remove();
-			}
-			
-			Thread.sleep(1200);
-		
+
+	public void greenLight() throws InterruptedException {
+		straight.remove();
+		if (light.isRed()) {
+			turnLeft.remove();
+		}
+
+		Thread.sleep(1000);
+
 	}
-	public boolean getEmergencyStatus(){
+
+	public boolean getEmergencyStatus() {
 		return emergency.isEmpty();
 	}
-	
-	
+
 }
